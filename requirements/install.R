1131a5c6fa78c9a2387a4c9b1a3a37969b95ff44
molpopgen <>
2021-01-16 22:16:34 +0000

    molpopgen published a site update

--- requirements/install.R
+++
@@ -1,7 +0,0 @@
-install.packages(c("bookdown", 
-                   "rmarkdown",
-                   "reticulate"
-                   ),
-                 lib="~/.local/Rlibs",
-                 nCpus=2,
-                 ask=FALSE)
